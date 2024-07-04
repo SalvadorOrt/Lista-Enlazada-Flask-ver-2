762fbdf1dcb842faa2e29bfda92fa9c5fda19c94
SalvadorOrt <salvadorortega505@gmail.com>
2024-07-03 22:16:25 -0500

    lista

--- lista.py
+++ lista.py
@@ -2,3 +2,13 @@ class Nodo():
     def __init__(self,dato):
         self.elemento = dato
         self.next = None
+class Lista():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    def print(self):
+        temp = self.head
+        while(temp != None):
+            print(temp.elemento)
+            temp = temp.next
