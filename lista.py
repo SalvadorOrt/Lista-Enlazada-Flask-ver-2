7fc4a3fc196fbe5106ff89d975dac9fb085bb05b
SalvadorOrt <salvadorortega505@gmail.com>
2024-07-03 22:49:29 -0500

    método pop

--- lista.py
+++ lista.py
@@ -8,10 +8,13 @@ class Lista():
         self.tail = None
         self.length = 0
     def print(self):
+        cadena = ''
         temp = self.head
         while(temp != None):
+            cadena += str(temp.elemento) + ' '
             print(temp.elemento)
             temp = temp.next
+        return cadena
     def append(self,dato):
         nodo = Nodo(dato)
         if self.length == 0:
@@ -21,3 +24,18 @@ class Lista():
             self.tail.next = nodo
             self.tail = nodo
         self.length += 1
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next != None):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp.elemento
--- web.py
+++ web.py
@@ -1,12 +1,21 @@
 from flask import Flask,request,render_template
-
+from lista import Lista
 app = Flask(__name__)
+
+lista = Lista()
+
 @app.route('/',methods = ['GET','POST'])
 def root():
+    global lista
+    temporal = ''
     if request.method == 'POST':
         if "agregar_final" in request.form:
             valor = request.form["valor"]
+            lista.append(int(valor))
             print(valor)
-    return render_template('index.html')
+        if "eliminar_final" in request.form:
+            temporal = lista.pop()
+            print(temporal)
+    return render_template('index.html',lista = lista.print(),elemento_eliminado = temporal)
 if __name__ == '__main__':
     app.run(debug=True)
